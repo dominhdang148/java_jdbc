c546f6b44bb9545a62860979ecd8ae3c70aef416
dominhdang148 <myclone235@gmail.com>
2024-08-04 08:59:07 +0700

    connect successfully

--- src/main/java/com/dominhdang/app/App.java
+++ src/main/java/com/dominhdang/app/App.java
@@ -1,6 +1,14 @@
 package com.dominhdang.app;
 
+import com.dominhdang.app.DAO.StudentDAO;
+import com.dominhdang.app.Utils.DatabaseConnection;
+
 public class App {
   public static void main(String[] args) {
+    try {
+      StudentDAO studentDAO = new StudentDAO();
+    } finally {
+      DatabaseConnection.closeConnection();
+    }
   }
 }
--- src/main/java/com/dominhdang/app/DAO/StudentDAO.java
+++ src/main/java/com/dominhdang/app/DAO/StudentDAO.java
@@ -1,7 +1,13 @@
 package com.dominhdang.app.DAO;
 
+import java.sql.Connection;
+
+import com.dominhdang.app.Utils.DatabaseConnection;
+
 public class StudentDAO {
+  private Connection connection;
 
-  public static 
-
+  public StudentDAO() {
+    connection = DatabaseConnection.getConnection();
+  }
 }
